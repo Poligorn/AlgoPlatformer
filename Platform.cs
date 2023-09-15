443489a4c448ee6eb3c26adc5ffde1f9a18784d4
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-09-15 11:10:29 +0300

    Скрипт для летающей платформы
    Для работы необходимо создать саму платформу, два коллайдера и тегами обозначить триггеры, чтобы она только платформа реагировала на них

---
+++ Platform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public bool isActive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "StopPlatform")
+        {
+            direction *= -1;
+        }
+        if(other.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+}
