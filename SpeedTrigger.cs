17e4f2518027a267b6bcc78a9603b3f73b08ed79
Poligorn <133873347+Poligorn@users.noreply.github.com>
2023-11-13 19:01:15 +0300

    Add files via upload

---
+++ SpeedTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedTrigger : MonoBehaviour
+{
+    public float speedFactor = 2.5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Увеличение скорости бега игрока
+        other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //Уменьшение скорости бега игрока
+        other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
+    }
+}
+
